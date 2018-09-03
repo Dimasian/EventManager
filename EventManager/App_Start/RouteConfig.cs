7130e509378b504334821e675114a9995dd531b5
martid <martid1703@gmail.com>
2018-09-03 18:58:45 +0300

    ---working on authorization---03/09/18

--- EventManager/App_Start/RouteConfig.cs
+++ EventManager/App_Start/RouteConfig.cs
@@ -7,37 +7,37 @@ using System.Web.Routing;
 
 namespace EventManager
 {
-  public class RouteConfig
-  {
-    public static void RegisterRoutes(RouteCollection routes)
+    public class RouteConfig
     {
-      routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
 
-      routes.MapRoute(
-     name: "EventsCreate",
-     url: "Events/{userId}/Create",
-     defaults: new { controller = "Events", action = "Create"}
-     );
+            routes.MapRoute(
+               name: "EventsCreate",
+               url: "Events/{userId}/Create",
+               defaults: new { controller = "Events", action = "Create" }
+               );
 
-      routes.MapRoute(
-          name: "EventsByPage",
-          url: "Events/{userId}/Page{pageUserEvents}",
-          defaults: new
-          { controller = "Events", action = "IndexById" }
-          );
+            routes.MapRoute(
+                name: "EventsByPage",
+                url: "Events/{userId}/Page{pageUserEvents}",
+                defaults: new
+                { controller = "Events", action = "IndexById" }
+                );
 
-      routes.MapRoute(
-          name: "EventsbyCategory",
-          url: "Events/{userId}/{eventTypeId}",
-          defaults: new { controller = "Events", action = "IndexById", eventTypeId = UrlParameter.Optional }
-          );
+            routes.MapRoute(
+                name: "EventsbyCategory",
+                url: "Events/{userId}/{eventTypeId}",
+                defaults: new { controller = "Events", action = "IndexById", eventTypeId = UrlParameter.Optional }
+                );
 
-      routes.MapRoute(
-          name: "Default",
-          url: "{controller}/{action}/{id}",
-          defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-      );
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
     }
-  }
 }
--- EventManager/Models/EventMetaData.cs
+++ EventManager/Models/EventMetaData.cs
@@ -6,27 +6,29 @@ using System.ComponentModel.DataAnnotations;
 
 namespace EventManager
 {
-  [MetadataType(typeof(EventMetaData))]
-  public partial class Event
-  {
-  }
+    [MetadataType(typeof(EventMetaData))]
+    public partial class Event
+    {
+    }
 
-  public class EventMetaData
-  {
-    [Required]
-    [Display(Name = "Event Name")]
-    public string Name;
+    public class EventMetaData
+    {
+        [Required]
+        [Display(Name = "Event Name")]
+        public string Name;
 
-    [Required]
-    public System.DateTime Date { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:d}")]
+        public System.DateTime Date { get; set; }
 
-    [Required]
-    public int ManagerId { get; set; }
+        [Required]
+        public int ManagerId { get; set; }
 
-    [Required]
-    public int EventTypeId { get; set; }
+        [Required]
+        public int EventTypeId { get; set; }
 
 
-    public bool IsPublic { get; set; }
-  }
+        public bool IsPublic { get; set; }
+    }
 }
